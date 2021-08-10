5a711eced2ad426a63930c2268c18966d6c801b1
lkozina1309 <65762882+lkozina1309@users.noreply.github.com>
2021-08-10 16:07:51 +0200

    Add files via upload

---
+++ camera_gray.py
@@ -0,0 +1,23 @@
+import cv2
+from cv_bridge import CvBridge
+import rospy
+from sensor_msgs.msg import Image
+import numpy as np
+
+
+def callback(data):
+	br = CvBridge()
+	current_frame = br.imgmsg_to_cv2(data)
+	gray = cv2.cvtColor(current_frame,cv2.COLOR_BGR2GRAY)
+	cv2.imshow("camera", gray)
+	cv2.waitKey(1)
+	
+
+def listener():
+	rospy.init_node("video_sub_py", anonymous=True)
+	rospy.Subscriber('/webcam/image_raw', Image, callback)
+	rospy.spin()
+	cv2.destroyAllWindows()
+	
+if __name__ == "__main__":
+	listener()
